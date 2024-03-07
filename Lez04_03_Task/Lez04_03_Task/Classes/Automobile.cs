334f2a773e9c1b60cb3f28d6729e1f8cb385cc65
Matteo Cotza <matteocotza92@gmail.com>
2024-03-07 10:51:48 +0100

    Esercizio giorno 4, classi

---
+++ Lez04_03_Task/Lez04_03_Task/Classes/Automobile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lez04_03_Task.Classes
+{
+    internal class Automobile : Veicolo
+    {
+        public int NumPorte { get; set; }
+
+
+        public override void accensione()
+        {
+            Console.WriteLine("La macchina accellera");
+        }
+
+        public override void accellera()
+        {
+            Console.WriteLine("La macchina accellera");
+        }
+
+        public override void frena()
+        {
+            Console.WriteLine("La macchina accellera");
+        }
+
+        public Automobile(string? marca, int numporte)
+        {
+            Marca = marca;
+            NumPorte = numporte;
+        }
+    }
+}
---
+++ Lez04_03_Task/Lez04_03_Task/Classes/Veicolo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lez04_03_Task.Classes
+{
+    internal abstract class Veicolo
+    {
+        public string? Marca { get; set; }
+
+        public abstract void accensione();
+
+        public abstract void accellera();
+
+        public abstract void frena();
+    }
+}
