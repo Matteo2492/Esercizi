fa81bb21bdae18f420759dd8e15bad49e6455669
Matteo Cotza <matteocotza92@gmail.com>
2024-03-20 17:40:11 +0100

    Commit iniziale esercizio Biblioteca

---
+++ EsercizioBiblioteca/DB_05_Biblioteca/DB_05_Biblioteca/Program.cs
@@ -0,0 +1,34 @@
+﻿using DB_05_Biblioteca.DAL;
+using DB_05_Biblioteca.Models;
+
+namespace DB_05_Biblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSERIMENTO UTENTE
+            Utente utente = new Utente("Matteo", "Cotza", "m@m.com");
+            Console.WriteLine(UtenteDAL.getIstanza().Insert(utente) ? "Stappo!" : "Errore");
+
+            //INSERIMENTO LIBRO
+            Libro libro = new Libro("Il signore degli angelli", true);
+
+            //LETTURA UTENTE 
+            UtenteDAL utenteDAL = UtenteDAL.getIstanza();
+            List<Utente> listaUtenti = utenteDAL.GetAll();
+            Console.WriteLine(listaUtenti.Count());
+            Utente utenteIDzero = listaUtenti[2];
+
+            //UPDATE utente
+            utenteIDzero.Nome = "Matteuccio";
+            utenteDAL.Update(utenteIDzero);
+
+            //DELETE utente
+            //utenteDAL.Delete(utenteIDzero);
+
+
+
+        }
+    }
+}
